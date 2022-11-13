5fbe26c73a03edd3956e6a4b933f5123e63601dd
kirbu123 <56351167+kirbu123@users.noreply.github.com>
2022-11-14 01:56:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from random import randint, choice
 screen = turtle.Screen()
 screen.title('Labirint game')
 screen.bgcolor('black')
-screen.setup(605, 605)
+screen.setup(620, 620)
 screen.tracer(0)
 
 # labirint maker
@@ -26,41 +26,42 @@ def Board_index(grid, gamer):
 
 def Make_grid():
     global grid
-    x = -270
-    y = 270
+    x = -280
+    y = 280
     i = False
-    while (y > -270):
+    while (y > -280):
         if i == True:
             i = False
         else:
             i = True
-        while (x < 270):
-            cube = turtle.Turtle()
-            cube.color('white')
-            cube.shape('square')
-            cube.shapesize(1, 1, 1)
-            cube.penup()
-            cube.goto(x, y)
-            grid.append(cube)
+        while (x < 280):
+            if (x**2 + y**2 != 0):
+                cube = turtle.Turtle()
+                cube.color('white')
+                cube.shape('square')
+                cube.shapesize(1, 1, 1)
+                cube.penup()
+                cube.goto(x, y)
+                grid.append(cube)
             if i:
                 x += 20
             else:
                 x += 40
         y -= 20
         if i:
-            x = -250
+            x = -260
         else:
-            x = -270
+            x = -280
 
     build = turtle.Turtle()
     build.hideturtle()
     build.color('orange')
     build.penup()
-    build.goto(-270, 250)
+    build.goto(-280, 260)
     fordelete = []
-    while (build.position()[1] > -270):
+    while (build.position()[1] > -280):
         print(build.position()[1])
-        while (build.position()[0] < 270):
+        while (build.position()[0] < 280):
             helper = []
             build.goto(build.position()[0], build.position()[1] + 20)
             if Board(grid, build):
@@ -71,7 +72,7 @@ def Make_grid():
             build.goto(build.position()[0] + 20, build.position()[1])
             if len(helper) > 1:
                 fordelete.append(choice(helper))
-        build.goto(-270, build.position()[1] - 40)
+        build.goto(-280, build.position()[1] - 40)
     build.reset()
 
     helper = []
@@ -87,34 +88,42 @@ def Make_grid():
 
 Make_grid()
 
+def Update_gamer():
+    global gamer
+    gamer.reset()
+    gamer = turtle.Turtle()
+    gamer.color('red')
+    gamer.shape('square')
+    gamer.shapesize(0.9, 0.9, 1)
+    gamer.penup()
+    gamer.goto(-300, 300)
+
 gamer = turtle.Turtle()
-gamer.color('red')
-gamer.shape('square')
-gamer.shapesize(0.9, 0.9, 1)
-gamer.penup()
-gamer.goto(-290, 290)
+
+Update_gamer()
+
 
 
 def Move(gamer, grid, dir):
-    if dir == "Up" and gamer.position()[1] < 290:
+    if dir == "Up" and gamer.position()[1] < 300:
         gamer.setheading(90)
         gamer.forward(20)
         if Board(grid, gamer):
             gamer.setheading(270)
             gamer.forward(20)
-    if dir == "Down" and gamer.position()[1] > -290:
+    if dir == "Down" and gamer.position()[1] > -300:
         gamer.setheading(270)
         gamer.forward(20)
         if Board(grid, gamer):
             gamer.setheading(90)
             gamer.forward(20)
-    if dir == "Left" and gamer.position()[0] > -290:
+    if dir == "Left" and gamer.position()[0] > -300:
         gamer.setheading(180)
         gamer.forward(20)
         if Board(grid, gamer):
             gamer.setheading(0)
             gamer.forward(20)
-    if dir == "Right" and gamer.position()[0] < 290:
+    if dir == "Right" and gamer.position()[0] < 300:
         gamer.setheading(0)
         gamer.forward(20)
         if Board(grid, gamer):
@@ -134,7 +143,7 @@ screen.onkeypress(lambda: Move(gamer, grid, 'Up'), 'Up')
 screen.onkeypress(lambda: Move(gamer, grid, 'Down'), 'Down')
 screen.onkeypress(lambda: Move(gamer, grid, 'Left'), 'Left')
 screen.onkeypress(lambda: Move(gamer, grid, 'Right'), 'Right')
-screen.onkeypress(lambda: gamer.goto(-290, 290), 'a')
+screen.onkeypress(lambda: gamer.goto(-300, 300), 'a')
 screen.onkeypress(lambda: Stop(), 'b')
 
 screen.listen()
@@ -164,7 +173,7 @@ while RunWhile:
         Clear()
         grid = []
         Make_grid()
-        gamer.goto(-290, 290)
+        Update_gamer()
         screen.bgcolor('black')
         Update_win()
     screen.update()
